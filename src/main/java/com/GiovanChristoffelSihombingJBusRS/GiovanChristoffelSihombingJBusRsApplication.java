95e20761f3b318939882638c08acbf7505be04e9
MorpKnight <christoffelsihombing@gmail.com>
2023-11-24 12:33:22 +0700

    CS9 - jar file

--- src/main/java/com/GiovanChristoffelSihombingJBusRS/GiovanChristoffelSihombingJBusRsApplication.java
+++ src/main/java/com/GiovanChristoffelSihombingJBusRS/GiovanChristoffelSihombingJBusRsApplication.java
@@ -1,13 +1,13 @@
-package com.GiovanChristoffelSihombingJBusRS;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GiovanChristoffelSihombingJBusRsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GiovanChristoffelSihombingJBusRsApplication.class, args);
-	}
-
-}
+//package com.GiovanChristoffelSihombingJBusRS;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class GiovanChristoffelSihombingJBusRsApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GiovanChristoffelSihombingJBusRsApplication.class, args);
+//	}
+//
+//}
--- src/main/java/com/GiovanChristoffelSihombingJBusRS/controller/AccountController.java
+++ src/main/java/com/GiovanChristoffelSihombingJBusRS/controller/AccountController.java
@@ -5,32 +5,29 @@ import com.GiovanChristoffelSihombingJBusRS.Algorithm;
 import com.GiovanChristoffelSihombingJBusRS.Renter;
 import com.GiovanChristoffelSihombingJBusRS.dbjson.JsonAutowired;
 import com.GiovanChristoffelSihombingJBusRS.dbjson.JsonTable;
+
 import java.security.MessageDigest;
 
 import org.springframework.web.bind.annotation.*;
 
 
-
-
-
 @RestController
 @RequestMapping("/account")
-public class AccountController implements BasicGetController<Account>
-{
+public class AccountController implements BasicGetController<Account> {
     @JsonAutowired(value = Account.class, filepath = "src/main/java/com/GiovanChristoffelSihombingJBusRS/json/account.json")
     public static JsonTable<Account> accountTable;
 
-    public JsonTable<Account> getJsonTable(){
+    public JsonTable<Account> getJsonTable() {
         return accountTable;
     }
 
-    public static String hashPassword(String password){
+    public static String hashPassword(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(password.getBytes());
             byte[] digest = md.digest();
             StringBuilder sb = new StringBuilder();
-            for(byte b : digest){
+            for (byte b : digest) {
                 sb.append(String.format("%02x", b & 0xff));
             }
             return sb.toString();
@@ -41,25 +38,26 @@ public class AccountController implements BasicGetController<Account>
     }
 
     @GetMapping
-    String index() { return "account page"; }
+    String index() {
+        return "account page";
+    }
 
     @PostMapping("/register")
     BaseResponse<Account> register
             (
-                    // @RequestParam String name,
-                    // @RequestParam String email,
-                    // @RequestParam String password
-                    @ModelAttribute BaseAccount baseAccount
-            )
-    {
+                    @RequestParam String name,
+                    @RequestParam String email,
+                    @RequestParam String password
+//                    @ModelAttribute BaseAccount baseAccount
+            ) {
         try {
-            Account account = new Account(baseAccount.name, baseAccount.email, baseAccount.password);
+            Account account = new Account(name, email, password);
 
-            if(baseAccount.name.isBlank() || !account.validate() || Algorithm.<Account>exists(accountTable, b -> b.email.equals(baseAccount.email))){
+            if (name.isBlank() || !account.validate() || Algorithm.<Account>exists(accountTable, b -> b.email.equals(email))) {
                 return new BaseResponse<>(false, "Gagal register", null);
             }
-            
-            account.password = hashPassword(baseAccount.password);
+
+            account.password = hashPassword(password);
             accountTable.add(account);
             return new BaseResponse<>(true, "Berhasil register", account);
         } catch (Exception e) {
@@ -69,14 +67,15 @@ public class AccountController implements BasicGetController<Account>
 
     @PostMapping("/login")
     BaseResponse<Account> login(
-        // @RequestParam String email,
-        // @RequestParam String password
-        @ModelAttribute BaseAccount baseAccount
+            @RequestParam String email,
+            @RequestParam String password
+//        @ModelAttribute BaseAccount baseAccount
     ) {
         try {
-            String hashedPassword = hashPassword(baseAccount.password);
-            Account account = Algorithm.<Account>find(accountTable, b -> b.email.equals(baseAccount.email) && b.password.equals(hashedPassword));
-            if(account == null){
+//            String hashedPassword = hashPassword(baseAccount.password);
+            String hashedPassword = hashPassword(password);
+            Account account = Algorithm.<Account>find(accountTable, b -> b.email.equals(email) && b.password.equals(hashedPassword));
+            if (account == null) {
                 return new BaseResponse<>(false, "Akun tidak ditemukan", null);
             }
 
@@ -88,15 +87,15 @@ public class AccountController implements BasicGetController<Account>
 
     @PostMapping("/{id}/registerRenter")
     BaseResponse<Renter> registerRenter(
-        // @PathVariable int id,
-        // @RequestParam String companyName,
-        // @RequestParam String address,
-        // @RequestParam String phoneNumber
-        @ModelAttribute BaseAccount baseAccount
+            // @PathVariable int id,
+            // @RequestParam String companyName,
+            // @RequestParam String address,
+            // @RequestParam String phoneNumber
+            @ModelAttribute BaseAccount baseAccount
     ) {
         try {
             Account account = getById(baseAccount.id);
-            if(account == null || account.company != null){
+            if (account == null || account.company != null) {
                 return new BaseResponse<>(false, "Gagal register renter", null);
             }
 
@@ -110,13 +109,13 @@ public class AccountController implements BasicGetController<Account>
 
     @PostMapping("/{id}/topUp")
     BaseResponse<Double> topUp(
-        // @PathVariable int id,
-        // @RequestParam double amount
-        @ModelAttribute BaseAccount baseAccount
+            // @PathVariable int id,
+            // @RequestParam double amount
+            @ModelAttribute BaseAccount baseAccount
     ) {
         try {
             Account account = getById(baseAccount.id);
-            if(account == null || baseAccount.amount <= 0){
+            if (account == null || baseAccount.amount <= 0) {
                 return new BaseResponse<>(false, "Gagal top up", account.balance);
             }
 
